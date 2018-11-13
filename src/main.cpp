c743a1a5255adbad0d13629ba79edf5362da4750
CafieneAdikt <tearacepeterson13@gmail.com>
2018-11-13 19:33:21 +1300

    added 3 states

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ const int INTBUTTON3 = 6; // pause button
 volatile bool UP_Pressed = false; // flag for up button
 volatile bool DWN_Pressed = false; // flag for down button
 volatile bool PAUSE_Pressed = false; // flag for pause button
-const unsigned long DEBOUNCE_TIME_MS = 200; // debounce time interval
+const unsigned long DEBOUNCE_TIME_MS = 500; // debounce time interval
 unsigned long previousDebounce = 0; // define and set variable to zero
 unsigned long previousMillis = 0; // define and set variable to zero
 
@@ -55,13 +55,95 @@ void STARTscreen(void) {
   display.clearDisplay();
   display.println("Fleeing    Ninja");
   display.display();
-  delay(1000);
+  delay(500);
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(18,45);
   display.println("Press Any Button");
   display.display();
+  delay(1000);
 
+}
+
+void PAUSEscreen(void) {
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(25,10);
+  display.clearDisplay();
+  display.println("PAUSE");
+  display.display();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(15,45);
+  display.println("SCORE:");
+  display.display();
+  delay(2000);
+
+}
+
+
+void charactergrounded(void) {
+    // head
+    display.drawCircle(20, 35, 5, WHITE);
+
+    // body
+    display.drawPixel(20,41,WHITE);
+    display.drawPixel(20,42,WHITE);
+    display.drawPixel(20,43,WHITE);
+    display.drawPixel(20,44,WHITE);
+    display.drawPixel(20,45,WHITE);
+    display.drawPixel(20,46,WHITE);
+    display.drawPixel(20,47,WHITE);
+    display.drawPixel(20,48,WHITE);
+    display.drawPixel(20,49,WHITE);
+    display.drawPixel(20,50,WHITE);
+    display.drawPixel(20,51,WHITE);
+    display.drawPixel(20,52,WHITE);
+    display.drawPixel(20,53,WHITE);
+    display.drawPixel(20,54,WHITE);
+
+    // left leg
+    display.drawPixel(19,55,WHITE);
+    display.drawPixel(19,56,WHITE);
+    display.drawPixel(18,57,WHITE);
+    display.drawPixel(18,58,WHITE);
+    display.drawPixel(17,59,WHITE);
+    display.drawPixel(17,60,WHITE);
+    display.drawPixel(16,61,WHITE);
+    display.drawPixel(16,62,WHITE);
+    display.drawPixel(15,63,WHITE);
+
+    // right leg
+    display.drawPixel(21,55,WHITE);
+    display.drawPixel(21,56,WHITE);
+    display.drawPixel(22,57,WHITE);
+    display.drawPixel(22,58,WHITE);
+    display.drawPixel(23,59,WHITE);
+    display.drawPixel(23,60,WHITE);
+    display.drawPixel(24,61,WHITE);
+    display.drawPixel(24,62,WHITE);
+    display.drawPixel(25,63,WHITE);
+
+    // left arm
+    display.drawPixel(19,47,WHITE);
+    display.drawPixel(18,47,WHITE);
+    display.drawPixel(17,47,WHITE);
+    display.drawPixel(16,47,WHITE);
+    display.drawPixel(15,47,WHITE);
+    display.drawPixel(14,47,WHITE);
+
+    // right arm
+    display.drawPixel(21,47,WHITE);
+    display.drawPixel(22,47,WHITE);
+    display.drawPixel(23,47,WHITE);
+    display.drawPixel(24,47,WHITE);
+    display.drawPixel(25,47,WHITE);
+    display.drawPixel(26,47,WHITE);
+
+    // display character
+    display.display();
+    delay(1);
+    
 }
 
 
@@ -99,22 +181,25 @@ void loop() {
       // if any button is pressed start game
       if ((UP_Pressed &&
       ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) || (DWN_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) || (PAUSE_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS))) {
-    previousDebounce = currentmillis;   // set new debounce time
-    current_state = GAME_screen; // go to game screen
-    UP_Pressed = false; // reset flags
-    DWN_Pressed = false;
-    PAUSE_Pressed = false; 
+      previousDebounce = currentmillis;   // set new debounce time
+      display.clearDisplay(); // clear screen
+      current_state = GAME_screen; // go to game screen
+      UP_Pressed = false; // reset flags
+      DWN_Pressed = false;
+      PAUSE_Pressed = false; 
       }
       break;
 
     case GAME_screen:
       // actions
+      charactergrounded();
 
       // if some event then state change
       if (PAUSE_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) {
-    previousDebounce = currentmillis;   // set new debounce time
-    current_state = PAUSE_screen; // go to pause screen
-    PAUSE_Pressed = false; // reset flag
+      previousDebounce = currentmillis;   // set new debounce time
+      display.clearDisplay(); // clear screen
+      current_state = PAUSE_screen; // go to pause screen
+      PAUSE_Pressed = false; // reset flag
       }
       // if you die in game go to end screen
 
@@ -122,18 +207,20 @@ void loop() {
 
     case PAUSE_screen:
       // actions
+      PAUSEscreen(); 
 
       // if some event then state change
       if (PAUSE_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) {
-    previousDebounce = currentmillis;   // set new debounce time
-    current_state = GAME_screen; // go back to game srceen state
-    PAUSE_Pressed = false; // reset flag
+      previousDebounce = currentmillis;   // set new debounce time
+      display.clearDisplay(); // clear screen
+      current_state = GAME_screen; // go back to game srceen state
+      PAUSE_Pressed = false; // reset flag
       }
       // if down button pressed end game
       if (DWN_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) {
-    previousDebounce = currentmillis;   // set new debounce time
-    current_state = GAME_screen; // go to start screen
-    DWN_Pressed = false; // reset flag
+      previousDebounce = currentmillis;   // set new debounce time
+      current_state = GAME_screen; // go to start screen
+      DWN_Pressed = false; // reset flag
       }
       break;
 
@@ -142,16 +229,18 @@ void loop() {
 
       // if pause play button is pressed play another game
       if (PAUSE_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) {
-    previousDebounce = currentmillis;   // set new debounce time
-    current_state = GAME_screen;
-    PAUSE_Pressed = false; // reset flag
+      previousDebounce = currentmillis;   // set new debounce time
+      display.clearDisplay(); // clear screen
+      current_state = GAME_screen;
+      PAUSE_Pressed = false; // reset flag
       }
       // if up of down button is pressed go to start sceen
       if ((UP_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS)) || (DWN_Pressed && ((currentmillis - previousDebounce) >= DEBOUNCE_TIME_MS))) {
-    previousDebounce = currentmillis;   // set new debounce time
-    current_state = START_screen;
-    UP_Pressed = false; // reset flag
-    DWN_Pressed = false; // reset flag
+      previousDebounce = currentmillis;   // set new debounce time
+      display.clearDisplay(); // clear screen
+      current_state = START_screen;
+      UP_Pressed = false; // reset flag
+      DWN_Pressed = false; // reset flag
       }
       break;
   }
